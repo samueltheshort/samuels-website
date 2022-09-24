64d09cf7fcc82d16c7e20adeb0c24ca10f27a33b
samueltheshort <samueldekorte@outlook.com>
2022-09-24 16:43:54 +0200

    Index filled

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -255,42 +255,19 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I’m Samuel, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Samuel, a historian based in Utrecht, the Netherlands. I specialze in black American soldiers during the Second World War and Dutch soldiers during the Napoleonic wars."
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Boeken. Ik schrijf ze. <br />
-            Systemen. Ik fix ze.
+            I write books about the past, the present and the future!
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            Ik ben Samuel, een historicus annex schrijver annex tech god uit
-            Utrecht. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            My name is Samuel de Korte. As a historian I specialize in black American soldiers during the Second World War and Dutch soldiers during the Napoleonic wars. Past projects include writing the history of the 614th Tank Destroyer Battalion, the first segregated combat unit to win the Distinguished Unit Citation. Currently I'm working on a translation of a Dutch officer that served with Napoleon in Russia in 1812 and a book about the Tuskegee Airmen.
           </p>
           <div className="mt-6 flex gap-6">
-            <SocialLink
-              href="https://twitter.com"
-              aria-label="Follow on Twitter"
-              icon={TwitterIcon}
-            />
-            <SocialLink
-              href="https://www.instagram.com/samuel.dekorte/"
-              aria-label="Follow on Instagram"
-              icon={InstagramIcon}
-            />
-            <SocialLink
-              href="https://github.com"
-              aria-label="Follow on GitHub"
-              icon={GitHubIcon}
-            />
-            <SocialLink
-              href="https://www.linkedin.com/in/samueldekorte/"
-              aria-label="Follow on LinkedIn"
-              icon={LinkedInIcon}
-            />
+            
           </div>
         </div>
       </Container>
