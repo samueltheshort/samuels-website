eede3f4fa52afa6a52b287dcc70d5d2597edb138
samueltheshort <samueldekorte@outlook.com>
2022-09-24 12:42:47 +0200

    Add standard blog template

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -261,8 +261,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Boeken. Ik schrijf ze. <br />
-            Systemen. Ik fix ze.
+            Samuel de Korte is a writer from the Netherlands
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             Ik ben Samwell, een historicus annex schrijver annex tech god uit
