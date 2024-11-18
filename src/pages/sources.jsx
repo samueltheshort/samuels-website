5ac2dd24354ade51b086c576e37e521131482883
samueltheshort <samueldekorte@outlook.com>
2024-11-18 17:14:12 +0100

    Added blogpost and sources

--- src/pages/sources.jsx
+++ src/pages/sources.jsx
@@ -20,6 +20,8 @@ export default function Sources() {
           >
             The Golden Cannon: A History of the 969th Field Artillery Battalion (PDF). 
           </a>
+          <p className="text-sm text-gray-600"> The unit history of the 969th Field Artillery Battalion. The unit history contains encounters, songs, stories and photographs.
+          </p>
         </div>
         <div className="mt-8">
           <a
@@ -29,6 +31,20 @@ export default function Sources() {
           >
             Come Out Fighting: The Epic Tale of the 761st Tank Battalion (PDF). 
           </a>
+          <p className="text-sm text-gray-600"> The unit history of the 761st Tank Battalion also known as the Black Panthers. The unit history contains stories and images of this unit.
+  </p>
+        </div>
+        <div className="mt-8">
+          <a
+            href="/HelenDouglas.pdf"
+            download="HelenDouglas.pdf"
+            className="text-blue-500 hover:underline"
+          >
+            The Negro Soldier: A partial record of Negro devotion and heroism in the cause of freedom gathered from the files of the War and Navy Departments (PDF). 
+          </a>
+          <p className="text-sm text-gray-600">
+    A speech given by Helen Douglas in 1946. The speech provides a broad overview of the contributions of Black American soldiers during World War II.
+  </p>
         </div>
       </SimpleLayout>
     </>
