72984a02b6ec9261c38e22752e83041a4e7a63bd
samueltheshort <samueldekorte@outlook.com>
2024-11-25 08:50:13 +0100

    Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://samueldekorte.com', // Replace with your site's URL
+    generateRobotsTxt: true, // Optionally generate a robots.txt file
+    sitemapSize: 5000, // Optional, splits large sitemaps into multiple files
+  };
