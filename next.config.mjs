1aec63ca9d95de0cc1c019c861d167937cf9cb0b
samueltheshort <samueldekorte@outlook.com>
2024-12-11 17:56:38 +0100

    Updated node

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,7 @@ const nextConfig = {
   pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
   experimental: {
-    newNextLinkBehavior: true,
     scrollRestoration: true,
-    images: {
-      allowFutureImage: true,
-    },
   },
 };
 
