41978754fa4c3155a641e3cc74dd2eb6289110ca
samueltheshort <samueldekorte@outlook.com>
2024-12-21 19:21:13 +0100

    Updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,40 +1,52 @@
 module.exports = {
-    siteUrl: 'https://samueldekorte.com',
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    changefreq: 'daily',
-    priority: 0.7, // Set a default priority for all pages
-    additionalSitemaps: [
-      'https://samueldekorte.com/sitemap-0.xml',
-      'https://samueldekorte.com/sitemap-images.xml',
-    ],
-    // Define priorities for specific pages
-    transform: async (config, path) => {
-      if (path === '/') {
-        return {
-          loc: path,
-          changefreq: 'daily',
-          priority: 1.0,
-        };
-      } else if (path.includes('/about')) {
-        return {
-          loc: path,
-          changefreq: 'weekly',
-          priority: 0.9,
-        };
-      }
-      else if (path.includes('/articles')) {
-        return {
-          loc: path,
-          changefreq: 'weekly',
-          priority: 0.9,
-        };
-      }
+  siteUrl: 'https://samueldekorte.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7, // Default priority
+  additionalSitemaps: [
+    'https://samueldekorte.com/sitemap-0.xml',
+    'https://samueldekorte.com/sitemap-images.xml',
+  ],
+  transform: async (config, path) => {
+    // Define specific priorities and changefreqs for paths
+    if (path === '/') {
+      return {
+        loc: path,
+        changefreq: 'daily',
+        priority: 1.0,
+      };
+    } else if (path === '/about') {
+      return {
+        loc: path,
+        changefreq: 'weekly',
+        priority: 0.9,
+      };
+    } else if (path === '/articles') {
+      return {
+        loc: path,
+        changefreq: 'weekly',
+        priority: 0.9,
+      };
+    } else if (path.startsWith('/articles/')) {
       return {
         loc: path,
         changefreq: 'monthly',
         priority: 0.5,
       };
-    },
-  };
-  
+    }
+
+    // Default for all other paths
+    return {
+      loc: path,
+      changefreq: 'monthly',
+      priority: 0.5,
+    };
+  },
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://samueldekorte.com/sitemap-0.xml',
+      'https://samueldekorte.com/sitemap-images.xml',
+    ],
+  },
+};
